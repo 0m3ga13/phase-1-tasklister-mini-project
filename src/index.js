0154ca90914055f763304ebc5205c0fda754988c
0m3ga13 <barkaoussama13@gmail.com>
2023-08-11 18:52:26 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+  const descriptionInput = document.getElementById("new-task-description"); 
+  const taskList = document.getElementById("tasks"); 
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();   
+    const description = descriptionInput.value;
+    const listItem = document.createElement("li");
+    listItem.textContent = description;
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    deleteButton.style.fontSize = "12px"
+    deleteButton.style.marginLeft = "5rem"
+    deleteButton.style.background = "red"
+    deleteButton.addEventListener("click", function() {
+      taskList.removeChild(listItem);
+    });
+
+    listItem.appendChild(deleteButton);
+    taskList.appendChild(listItem)
+    descriptionInput.value = "";
+  });
+
+
 });
