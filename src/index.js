3a26ee8bda5b364459af6fa1d98bd905dc7093b4
0m3ga13 <barkaoussama13@gmail.com>
2023-08-12 01:05:22 +0100

    done

--- src/index.js
+++ src/index.js
@@ -3,19 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
 
   const descriptionInput = document.getElementById("new-task-description");
-
   const taskList = document.getElementById("tasks");
 
-  const sortAscendingButton = document.getElementById("sort-ascending");
-  sortAscendingButton.style.fontSize = "14px";
 
-  const sortDescendingButton = document.getElementById("sort-descending");
-  sortDescendingButton.style.fontSize = "14px";
-
-  sortDescendingButton.style.marginBottom = "1rem";
+  const toggleSort = document.getElementById("sort");
 
 
-  //we create an array in the outer scope
+  const prioritySelect = document.getElementById("priority-select")
+
   const tasksArray = [];
 
   //this function : clear the task list and creates and appends based on the task array
@@ -27,12 +22,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     tasksArray.forEach((task) => {
       const listItem = document.createElement("li");
-      listItem.textContent = task.description;
-      listItem.style.color =
-        task.priority === 1 ? "red" : task.priority === 2 ? "yellow" : "green";
-      listItem.appendChild(task.priorityValue);
+      listItem.classList.add("task-item"); 
+      const descriptionSpan = document.createElement("span");
+      descriptionSpan.textContent = task.description;
+      descriptionSpan.classList.add("task-description");
+      listItem.appendChild(descriptionSpan);
+      listItem.appendChild(task.editButton);
       listItem.appendChild(task.deleteButton);
       taskList.appendChild(listItem);
+      
+      descriptionSpan.style.color =
+        task.priority === 1 ? "red" : task.priority === 2 ? "yellow" : "green";
     });
   }
 
@@ -41,8 +41,6 @@ document.addEventListener("DOMContentLoaded", () => {
   function createDeleteButton(description) {
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "X";
-    deleteButton.style.fontSize = "12px";
-    deleteButton.style.marginLeft = "1rem";
     deleteButton.style.background = "red";
 
     deleteButton.addEventListener("click", function () {
@@ -57,64 +55,54 @@ document.addEventListener("DOMContentLoaded", () => {
     return deleteButton
   }
 
+  // function to create delete Button
+  function createEditButton(description) {
+    const editbButton = document.createElement("button");
+    editbButton.textContent = "Edit";
 
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
+    editbButton.style.background = "blue";
 
-    const description = descriptionInput.value;
-    let priority = 1;
-
-    const deleteButton = createDeleteButton(description);
-    
-    const priorityValue = document.createElement("select");
-    priorityValue.style.marginLeft = "5rem";
-
-    const highPriority = document.createElement("option");
-    highPriority.value = "1";
-    highPriority.textContent = "High Priority";
-    priorityValue.appendChild(highPriority);
-
-    const mediumPriority = document.createElement("option");
-    mediumPriority.value = "2";
-    mediumPriority.textContent = "Medium priority";
-    priorityValue.appendChild(mediumPriority);
-
-    const lowPriority = document.createElement("option");
-    lowPriority.textContent = "Low priority";
-    lowPriority.value = "3";
-    priorityValue.appendChild(lowPriority);
-
-    priorityValue.addEventListener("change", () => {
-      priority = parseInt(priorityValue.value);
-      const taskIndex = tasksArray.findIndex(
-        (taskObj) => taskObj.description === description
-      );
-      if (taskIndex !== -1) {
-        tasksArray[taskIndex].priority = priority;
+    editbButton.addEventListener("click", function () {
+      const newDescription = prompt("Enter new description:", description);
+      const taskIndex = tasksArray.findIndex(taskObj => taskObj.description === description);
+      if (newDescription !== null && taskIndex !== -1) {
+        tasksArray[taskIndex].description = newDescription;
         renderTasks();
       }
     });
+    return editbButton
+  }
 
-
-
-    const task = { description, priority, deleteButton, priorityValue };
-
-    tasksArray.push(task);
-
+  
+  let sort = true
+  //descending order
+  toggleSort.addEventListener("click", () => {
+    if (sort) 
+    tasksArray.sort((a, b) => a.priority - b.priority);
+    else 
+    tasksArray.sort((a, b) => b.priority - a.priority);
+    sort = !sort
     renderTasks();
+  });
 
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+  
+    const description = descriptionInput.value;
+    const priority = parseInt(prioritySelect.value)
+    const deleteButton = createDeleteButton(description);
+    const editButton = createEditButton(description);
+    
+    const task = { description, priority , deleteButton, editButton };
+  
+    tasksArray.push(task); 
+  
+      
+    renderTasks();
+  
     descriptionInput.value = "";
   });
+  
 
-  //ascending order
-  sortAscendingButton.addEventListener("click", () => {
-    tasksArray.sort((a, b) => b.priority - a.priority);
-    renderTasks();
-  });
-
-  //descending order
-  sortDescendingButton.addEventListener("click", () => {
-    tasksArray.sort((a, b) => a.priority - b.priority);
-    renderTasks();
-  });
 });
